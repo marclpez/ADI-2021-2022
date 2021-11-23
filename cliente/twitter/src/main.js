e5a1d3a4aeb3a32684226cf50cde7bb126a393e5
Marc López Espinosa <mle16@alu.ua.es>
2021-11-23 10:40:09 +0100

    Configuración del proyecto

--- cliente/twitter/src/main.js
+++ cliente/twitter/src/main.js
@@ -1,4 +1,16 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue' //import Vue
 import App from './App.vue'
+//import {router} from './router'
+//import Vuex from 'vuex';
+
+
+//Vue.use(Vuex);
 
 createApp(App).mount('#app')
+
+/**
+new Vue({
+    router,
+    store,
+    render: h => h(App)
+}).mount('#app'); */
---
+++ cliente/twitter/src/router.js
@@ -0,0 +1,19 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+
+
+Vue.use(Router);
+
+export const router = new Router({
+    mode: 'history',
+    routes: [
+        /**{
+            path: '/login',
+            component: Login
+        },
+        {
+            path: '/logout',
+            component: Logout
+        }*/
+    ]
+})
